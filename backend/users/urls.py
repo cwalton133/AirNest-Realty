f9731b264f204337f5d303eca15648433b82be95
Charles Walton <pythontutorials36@gmail.com>
2025-03-04 16:01:27 +0100

    Link to showcase views was modify

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path
-from . import views
-from .views import UserRegisterAPIView, UserLoginAPIView, PasswordResetListCreateAPIView
-
+from rest_framework_simplejwt.views import TokenRefreshView
+from users.views import RegisterAPIView, LoginAPIView, LogoutAPIView, GoogleLoginAPIView, UserProfileAPIView
 
 urlpatterns = [
+    path('register/', RegisterAPIView.as_view(), name="register"),
+    path('login/', LoginAPIView.as_view(), name="login"),
+    path('logout/', LogoutAPIView.as_view(), name="logout"),
+    path('profile/', UserProfileAPIView.as_view(), name="profile"),
 
-    path(' ', views.Home, name='home'),
-    path('register/', views.RegisterView, name='register'),
-    path('login/', views.LoginView, name='login'),
-    path('logout/', views.LogoutView, name='logout'),
-    path('forgot-password/', views.ForgotPassword, name='forgot-password'),
-    path('password-reset-sent/<str:reset_id>/',
-         views.PasswordResetSent, name='password-reset-sent'),
-    path('reset-password/<str:reset_id>/',
-         views.ResetPassword, name='reset-password'),
-    path('register/', UserRegisterAPIView.as_view(), name='user-register'),
-    path('login/', UserLoginAPIView.as_view(), name='user-login'),
-    path('password_reset/', PasswordResetListCreateAPIView.as_view(),
-         name='password-reset-list-create'),
+    # JWT Authentication
+    path('token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+
+    # Google OAuth2 Login
+    path('google-login/', GoogleLoginAPIView.as_view(), name="google-login"),
 ]
