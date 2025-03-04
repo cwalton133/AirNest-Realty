19d38238fcd23acbf6be07bc1d306f832864e31c
Charles Walton <pythontutorials36@gmail.com>
2025-03-04 15:49:37 +0100

    Modify admin panel to register Users for API integration

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import PasswordReset
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from allauth.socialaccount.models import SocialAccount
 
-# Register your models here.
-admin.site.register(PasswordReset)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('id', 'username', 'email', 'role',
+                    'is_verified', 'is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+         'fields': ('role', 'phone_number', 'profile_picture', 'is_verified')}),
+    )
+
+
+# try:
+#     admin.site.register(SocialAccount)
+# except AlreadyRegistered:
+#     pass
+
+if not admin.site.is_registered(SocialAccount):
+    admin.site.register(SocialAccount)
+
+admin.site.register(CustomUser, CustomUserAdmin)
