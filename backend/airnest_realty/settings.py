224fca6e74b805505e4badf841f77df3e8a1a2c4
Charles Walton <164152127+cwalton133@users.noreply.github.com>
2025-04-01 10:05:50 -0700

    Update settings.py

--- backend/airnest_realty/settings.py
+++ backend/airnest_realty/settings.py
@@ -200,6 +200,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pythontutorials36@gmail.com'
+EMAIL_HOST_USER = 'someone@gmail.com'
 # Use environment variables for security
-EMAIL_HOST_PASSWORD = 'P@ssword123'
+EMAIL_HOST_PASSWORD = 'your-password'
